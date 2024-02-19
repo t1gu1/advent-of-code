9ca88f0807802932971ed2e8a009cce5ccec1ae6
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-02-18 21:55:34 -0500

    Finish day 1

--- Node2D.gd
+++ Node2D.gd
@@ -31,59 +31,22 @@ var txtNumbersMapped = {
 	
 func transformTxtNumberToNumber(line: String, isRecursion = false):
 	var numbersFound = {}
-	var totalCaracters = 0
-	
-	#if(!isRecursion): print("avant:   ", line)
-	#else: print("seconde: ", line)
-	
+
 	for txt in txtNumbersMapped:
 		var pos = line.find(txt)
 		if(pos != -1): 
 			numbersFound[pos] = txt
-			totalCaracters += len(txt)
 		
 	for txtpos in numbersFound:
 		var pos = line.find(numbersFound[txtpos], txtpos + 1)
 		if(pos != -1): 
 			numbersFound[pos] = numbersFound[txtpos]
-			totalCaracters += len(numbersFound[txtpos])
-		
-	#print(numbersFound)
-	#if(line == "twone"): 
-		#print(line)
-		#print("numbersFound: ", numbersFound)
 	
-	var numbersFoundInOrder = []
-	var range
-	
-	if (!isRecursion): range = range(0, 60)
-	else: range = range(60, 0, -1)
-	
-	#if(line == "7seven9"): print(range)
-	#if(len("nineigth") >= len(line)): print(line)
-	if(!isRecursion && line.find("twone") != -1): print(line)
-	#if(!isRecursion && len(numbersFound) == 2): print(line)
-	
-	for pos in range:
-		#print("iiii: ", i) 
-		#print('numbersFound: ', numbersFound)
-		if(numbersFound.has(pos)): 
-			line = line.erase(pos, len(numbersFound[pos])).insert(pos, txtNumbersMapped[numbersFound[pos]])
-			if(isRecursion || len(line) <= totalCaracters): break
+	for pos in range(0, 60):
+		if(numbersFound.has(pos)):
+			# lol simply replace the first caracter
+			line = line.erase(pos, 1).insert(pos, txtNumbersMapped[numbersFound[pos]])
 			
-		
-	#for txt in numbersFoundInOrder:
-		##print('THETXT: ', txt)
-		#var pos = line.find(txt)
-		#line = line.erase(pos, len(txt)).insert(pos, txtNumbersMapped[txt])
-	
-	
-	
-	if(!isRecursion): transformTxtNumberToNumber(line, true)
-	#else: 
-		#print("Après:   ", line)
-		#print(" ")
-		
 	return line
 	
 func _advent_of_code_1():
@@ -95,14 +58,5 @@ func _advent_of_code_1():
 		var strNumbers = str(numbers)
 		numbersSum += int(strNumbers[0] + strNumbers[-1])
 		
-	print(numbersSum)
+	print(numbersSum) # 55651
 
-# 54983 too low
-# 55504
-# 55203
-# 55680
-# 55782
-# 55231
-# 55782
-# 55783
-# 55651
