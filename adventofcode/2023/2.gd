251408be339c417ba789057d919e42a7ddbbaa20
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-02-19 01:10:43 -0500

    day 2 - Part 1 done

--- adventofcode/2023/2.gd
+++ adventofcode/2023/2.gd
@@ -2,5 +2,25 @@ extends Node2D
 
 func _ready():
 	var txtLines = GlobalScript.get_adventofcode_file_content(2023, 2)
+	var limitPerColor = { "red": 12, "green": 13, "blue": 14 }
+	var sumIds = 0
+	
+	for line: String in txtLines:
+		var splitedToArray = line.replace(";", ",").replace(":", ",").rsplit(",")
+		var id = int(splitedToArray[0])
+		
+		# Remove ID from array
+		splitedToArray.remove_at(0)
+		
+		var isValid = true
+		for element in splitedToArray:
+			var number = int(element)
 
-	print(txtLines) # Result: 55652
+			for color in limitPerColor:
+				if(element.contains(color) && number > limitPerColor[color]):
+					isValid = false
+					break
+		
+		if(isValid): sumIds += id
+			
+	print(sumIds) # Part1: 2164
--- adventofcode/GlobalScript.gd
+++ adventofcode/GlobalScript.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 func get_adventofcode_file_content(year, day):
-	print(str("res://adventofcode/", year, "/", day, ".txt"))
 	var file = FileAccess.open(str("res://adventofcode/", year, "/", day, ".txt"), FileAccess.READ)
 	
 	var lines = []
