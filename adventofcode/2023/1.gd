d6d0d1c6c8f25b472641c77c10281e84a339fae0
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-02-18 22:44:25 -0500

    Add comments

--- adventofcode/2023/1.gd
+++ adventofcode/2023/1.gd
@@ -11,20 +11,26 @@ var txtNumbersMapped = {
 	"eight": "8",
 	"nine": "9"
 }
-	
-func transformTxtNumberToNumber(line: String, isRecursion = false):
-	var numbersFound = {}
 
+# All the "complexe" txt to number logic is in transformTxtNumberToNumber
+func transformTxtNumberToNumber(line: String, isRecursion = false):
+	var numbersFound = {} # "onetwo" -> [{0: "one", 3: "two"}]
+
+	# Associate the string number to a position
 	for txt in txtNumbersMapped:
 		var pos = line.find(txt)
 		if(pos != -1): 
 			numbersFound[pos] = txt
 		
+	# Hardcoded check for a second same number in the string like "onethreeone"
+	# Huge risk but.. I don't care for that code test event
 	for txtpos in numbersFound:
 		var pos = line.find(numbersFound[txtpos], txtpos + 1)
 		if(pos != -1): 
 			numbersFound[pos] = numbersFound[txtpos]
 	
+	# Hardcoded loop to loop on each caracter of the word
+	# Could make a loop to get the longest word.. too lazy lol
 	for pos in range(0, 60):
 		if(numbersFound.has(pos)):
 			# lol simply replace the first caracter
@@ -37,8 +43,11 @@ func _ready():
 	var numbersSum = 0
 
 	for line in txtLines:
+		# The int clear all letters lol #nice
 		var numbers = int(transformTxtNumberToNumber(line))
+		
+		# Sum the first and the last int found in the string
 		var strNumbers = str(numbers)
 		numbersSum += int(strNumbers[0] + strNumbers[-1])
 		
-	print(numbersSum) # 55651
+	print(numbersSum) # Result: 55651
